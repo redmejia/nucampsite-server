f3d62be883e38c2ba0d6c889924bd8d39b341693
redmejia <preynaldorod@gmail.com>
2022-04-09 14:44:22 -0700

    workshop2

---
+++ models/partner.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const partnerSchema = new Schema({
+	name : {
+		type : String,
+		require : true,
+		unique: true
+	},
+	image : {
+		type : String,
+		require : true
+	},
+	featured : {
+		type: String,
+		require : true,
+		unique: true
+	},
+	description : {
+		type : String,
+		require : true
+	}
+},{
+    timestamps: true
+})
+
+const Partner = mongoose.model('Partner', partnerSchema);
+
+module.exports = Partner;
