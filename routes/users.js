5904f98ef85daa5d9bc3177abe8ecaa42110d5ff
redmejia <preynaldorod@gmail.com>
2022-04-17 00:10:09 -0700

    Passport Facebook

--- routes/users.js
+++ routes/users.js
@@ -51,11 +51,21 @@ router.post('/signup', cors.corsWithOptions, (req, res) => {
     }
   );
 });
+
 router.post('/login', cors.corsWithOptions,  passport.authenticate('local'), (req, res) => {
   const token = authenticate.getToken({ _id: req.user._id });
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.json({ success: true, token: token, status: 'You are successfully logged in!' });
+});
+
+router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
+  if (req.user) {
+    const token = authenticate.getToken({ _id: req.user._id });
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({ success: true, token: token, status: 'You are successfully logged in!' });
+  }
 });
 
 router.get('/logout', cors.corsWithOptions,  (req, res, next) => {
